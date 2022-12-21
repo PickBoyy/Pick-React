21e946767527142c066352378032e27b2d1a21be
Breno Virgílio Malheiros <brenovirgilio14@gmail.com>
2022-12-21 14:21:40 -0300

    props

--- src/components/planets/planet/index.js
+++ src/components/planets/planet/index.js
@@ -1,12 +1,14 @@
 import React from "react";
-import "./style.css"
+import GrayImg from "../../shared/gray_img";
 
-const Planet = () => {
+const Planet = (props) => {
     return (
         <div>
-            <h4>Mercurio</h4>
-            <p>Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres.</p>
-            <img class = "img" alt="Mercurio" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg" ></img>
+            <hr/>
+            <h4>{props.nome}</h4>
+            <hr/>
+            <p>{props.descricao}</p>
+            <GrayImg img_url ={props.img_url}/>,
         </div>
     )
 }
---
+++ src/components/shared/gray_img/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./style.css"
+
+const GrayImg = (props) => {
+    return (
+        <img class = "img" alt="planeta" src={props.img_url} ></img>
+    )
+}
+export default GrayImg
