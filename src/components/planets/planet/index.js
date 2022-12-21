9da76036e8756902d1049f305257edb3c3bf37db
Breno Virgílio Malheiros <brenovirgilio14@gmail.com>
2022-12-21 13:39:49 -0300

    criando componente

---
+++ src/components/planets/planet/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css"
+
+const Planet = () => {
+    return (
+        <div>
+            <h4>Mercurio</h4>
+            <p>Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres.</p>
+            <img class = "img" alt="Mercurio" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg" ></img>
+        </div>
+    )
+}
+export default Planet;
