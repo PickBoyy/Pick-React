14ca459c7153ec5b5f21aff215083849ef2af3c1
Breno Virgílio Malheiros <brenovirgilio14@gmail.com>
2022-12-21 12:43:03 -0300

    1.0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import HelloWorld from "./hello";
+import Lorem from "./components/lorem";
 
 function App() {
   return (
     <div>
       <HelloWorld/>
+      <Lorem/>
     </div>
   );
 }
---
+++ src/components/lorem.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class Lorem extends React.Component {
+    render() {
+        return <p>Gabriel é um faveladinho comedor de cream crak</p>
+    }
+}
+export default Lorem
