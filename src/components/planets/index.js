08c0a0521d001620b3035b65c20daf35b06ba6b5
Breno Virgílio Malheiros <brenovirgilio14@gmail.com>
2022-12-22 15:21:50 -0300

    alterante state

--- src/components/planets/index.js
+++ src/components/planets/index.js
@@ -26,10 +26,26 @@ class Planets extends React.Component {
       ]
     };
   }
+  removeLast = () => {
+    let new_planets = [...this.state.planets]
+    new_planets.pop()
+    this.setState(state => ({
+      planets: new_planets
+    }))
+  }
+  duplicateLastPlanet = () => {
+    let last_planet = this.state.planets[this.state.planets.length -1]
+    this.setState(state => ({
+      planets: [...this.state.planets, last_planet]
+    }))
+  }
+
   render() {
     return (
       <Fragment>
         <h3>Planet List</h3>
+        <button onClick={this.removeLast}>Remove Last</button>
+        <button onClick={this.duplicateLastPlanet}>Duplicate Last Planet</button>
         <hr/>
         {this.state.planets.map((planet) =>
         <Planet nome = {planet.nome}
