bc9a3cf0f2c489eed3e346c2a0ed958c465e40af
Breno Virgílio Malheiros <brenovirgilio14@gmail.com>
2022-12-22 11:11:25 -0300

    render X elements JSX

--- src/components/planets/planet/index.js
+++ src/components/planets/planet/index.js
@@ -3,29 +3,28 @@ import GrayImg from "../../shared/gray_img";
 import Descri from "../../shared/descri";
 
 const Planet = (props) => {
-    const nomes = ['a','b','c','d']
-    const satellites = nomes.map((n) => 
-    <li>Satélite {n}</li>
-    )
-  let title = ''
+  const nomes = ["a", "b", "c", "d","e","f"];
+
+  let title = "";
   if (props.title_whith_underline)
-    title = 
+    title = (
       <h4>
         <u>{props.nome}</u>
       </h4>
-    
+    );
   else title = <h4>{props.nome}</h4>;
   return (
     <div>
       {title}
       <hr />
       <Descri descri={props.descricao} link={props.link} />
-      <GrayImg img_url={props.img_url} gray={props.gray} />,
-
-      <h4>Satélites</h4>
+      <GrayImg img_url={props.img_url} gray={props.gray} />,<h4>Satélites</h4>
       <ul>
-        {satellites}
+        {nomes.map((n) => (
+          <li>Satélite {n}</li>
+        ))}
       </ul>
+      <hr />
     </div>
   );
 };
